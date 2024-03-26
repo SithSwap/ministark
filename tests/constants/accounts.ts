a2b634a6e75f426a18ef89558f24f311ccab49be
Raymond Baril <0x25cfb68@gmail.com>
2024-03-26 17:31:55 +0400

    fix: remove env vars

--- tests/constants/accounts.ts
+++ tests/constants/accounts.ts
@@ -1,6 +1,7 @@
 export default [
 	{
-		key: import.meta.env['VITE_ACCOUNT_001_KEY'],
-		address: import.meta.env['VITE_ACCOUNT_001_ADDRESS']
+		// key: import.meta.env['VITE_ACCOUNT_001_KEY'],
+		// address: import.meta.env['VITE_ACCOUNT_001_ADDRESS']
+		address: '0x076bc3DFc1B905969DAAD66cA4B8918c86f0B4715cea81b7f8dC60D408e95Ffa'
 	}
 ] as const;
--- tests/constants/rpc.ts
+++ tests/constants/rpc.ts
@@ -1,6 +1,9 @@
 import { ChainID } from '$src/network/network.js';
 
 export default {
-	[ChainID.Goerli]: import.meta.env['VITE_RPC_URL_GOERLI'],
-	[ChainID.Mainnet]: import.meta.env['VITE_RPC_URL_MAINNET']
+	// [ChainID.Goerli]: import.meta.env['VITE_RPC_URL_GOERLI'],
+	// [ChainID.Mainnet]: import.meta.env['VITE_RPC_URL_MAINNET']
+
+	[ChainID.Goerli]: 'https://starknet-goerli.g.alchemy.com/v2/n0Yg51cD7qDI_65QKVLSZcj9oeHQ3sGt',
+	[ChainID.Mainnet]: ''
 } as Partial<Record<ChainID, string>>;
